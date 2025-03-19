d50bc1f3d2d4bb1050f529971d9f8d575817971a
eliottvannuffel <85699497+eliottvannuffel@users.noreply.github.com>
2025-03-19 13:42:09 +0100

    Changes to be committed: modified:   Virus super dangereux.py new file:   youtube-uwuuuuu.wav

--- Virus super dangereux.py
+++ Virus super dangereux.py
@@ -1,1 +1,17 @@
-print("Coucou mon chaton")
+import os
+import sys
+import pygame
+from pynput import mouse
+
+def play_sound_on_mouse_move():
+    pygame.mixer.init()
+    sound = pygame.mixer.Sound('youtube-uwuuuuu.wav')
+    
+    def on_move(x, y):
+        sound.play()
+    
+    with mouse.Listener(on_move=on_move) as listener:
+        listener.join()
+    
+if __name__ == '__main__':
+    play_sound_on_mouse_move()
